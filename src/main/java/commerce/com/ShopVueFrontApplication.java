c77ec04180e08b5f83554bb918f80be16b7fbbf1
jayk-s <kyrie0216@ictway.co.kr>
2021-09-29 17:14:00 +0900

    init

---
+++ src/main/java/commerce/com/ShopVueFrontApplication.java
@@ -0,0 +1,13 @@
+package commerce.com;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShopVueFrontApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShopVueFrontApplication.class, args);
+	}
+
+}
---
+++ src/test/java/commerce/com/ShopVueFrontApplicationTests.java
@@ -0,0 +1,13 @@
+package commerce.com;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShopVueFrontApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
